e7da88056fa9c55ce61610554cfe1f16bc15cb81
Prithijit Das <55277163+rasoraj@users.noreply.github.com>
2019-10-29 09:59:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+
+function spreadgreen(){
+
+    TweenMax.to(".greenbox1", 0.75, {x:230});
+    TweenMax.to(".greenbox2", 0.75, {x:465});
+    TweenMax.to(".greenbox3", 0.75, {x:700});
+    TweenMax.to(".greenbox4", 0.75, {x:935});
+    TweenMax.to(".greenbox5", 0.75, {x:1170});
+}
+
+function spreadyellow(){
+
+    TweenMax.to(".yellowbox1", 0.75, {x:230});
+    TweenMax.to(".yellowbox2", 0.75, {x:465});
+    TweenMax.to(".yellowbox3", 0.75, {x:700});
+    TweenMax.to(".yellowbox4", 0.75, {x:935});
+    TweenMax.to(".yellowbox5", 0.75, {x:1170});
+}
+
+function spreadblue(){
+
+    TweenMax.to(".bluebox1", 0.75, {x:230});
+    TweenMax.to(".bluebox2", 0.75, {x:465});
+    TweenMax.to(".bluebox3", 0.75, {x:700});
+    TweenMax.to(".bluebox4", 0.75, {x:935});
+    TweenMax.to(".bluebox5", 0.75, {x:1170});
+}
+
+function fixedyellow()
+{
+    TweenMax.to(".yellowbox1", 0.75, {x:0});
+    TweenMax.to(".yellowbox2", 0.75, {x:0});
+    TweenMax.to(".yellowbox3", 0.75, {x:0});
+    TweenMax.to(".yellowbox4", 0.75, {x:0});
+    TweenMax.to(".yellowbox5", 0.75, {x:0});
+}
+
+function fixedblue()
+{
+    TweenMax.to(".bluebox1", 0.75, {x:0});
+    TweenMax.to(".bluebox2", 0.75, {x:0});
+    TweenMax.to(".bluebox3", 0.75, {x:0});
+    TweenMax.to(".bluebox4", 0.75, {x:0});
+    TweenMax.to(".bluebox5", 0.75, {x:0});
+}
+
+function fixedgreen()
+{
+    TweenMax.to(".greenbox1", 0.75, {x:0});
+    TweenMax.to(".greenbox2", 0.75, {x:0});
+    TweenMax.to(".greenbox3", 0.75, {x:0});
+    TweenMax.to(".greenbox4", 0.75, {x:0});
+    TweenMax.to(".greenbox5", 0.75, {x:0});
+}
+
+  
+
